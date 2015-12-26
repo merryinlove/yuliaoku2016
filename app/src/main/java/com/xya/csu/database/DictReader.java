dc92bf865f4b56a3086770a06c7b23d836355bf3
merryinlove <merryinlove@sina.com>
2015-12-26 17:14:22 +0800

    [feature] add query difference dictionary database

--- app/src/main/java/com/xya/csu/database/DictReader.java
+++ app/src/main/java/com/xya/csu/database/DictReader.java
@@ -3,6 +3,8 @@ package com.xya.csu.database;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.xya.csu.model.Oxford;
+import com.xya.csu.model.Yuliaoku;
 import com.xya.csu.yuliaoku.InitialActivity;
 
 import java.util.ArrayList;
@@ -13,13 +15,15 @@ import java.util.List;
  */
 public class DictReader {
 
+    private SQLiteDatabase database;
+
     public SQLiteDatabase open() {
         SQLiteDatabase database = SQLiteDatabase.openOrCreateDatabase(InitialActivity.external_path + InitialActivity.OXFORD, null);
         return database;
     }
 
     private DictReader() {
-
+        database = open();
     }
 
     private static class DictReaderHolder {
@@ -30,23 +34,58 @@ public class DictReader {
         return DictReaderHolder.dictReader;
     }
 
-    public String query(String key) {
-        SQLiteDatabase database = open();
-        String result = null;
-        Cursor cursor = database.rawQuery("select _value from  main where _key = ?", new String[]{key});
-        while (cursor.moveToNext()) {
-            result = cursor.getString(cursor.getColumnIndex("_value"));
+    public List<Object> query(String key, String dictionary) {
+        List<Object> result = new ArrayList<>();
+        Cursor cursor = null;
+        if (dictionary.toLowerCase().equals("yuliaoku".toLowerCase())) {
+            cursor = database.rawQuery("select * from " + dictionary + " where _entry = ?", new String[]{key});
+            while (cursor.moveToNext()) {
+                long _id = cursor.getLong(cursor.getColumnIndex("_id"));
+                String _entry = cursor.getString(cursor.getColumnIndex("_entry"));
+                String _phonetic = cursor.getString(cursor.getColumnIndex("_phonetic"));
+                String _interpretation = cursor.getString(cursor.getColumnIndex("_interpretation"));
+                String _formation = cursor.getString(cursor.getColumnIndex("_formation"));
+                String _quotation = cursor.getString(cursor.getColumnIndex("_quotation"));
+                String _translation = cursor.getString(cursor.getColumnIndex("_translation"));
+                String _author = cursor.getString(cursor.getColumnIndex("_author"));
+                String _title = cursor.getString(cursor.getColumnIndex("_title"));
+                String _source = cursor.getString(cursor.getColumnIndex("_source"));
+                String _time = cursor.getString(cursor.getColumnIndex("_time"));
+                String _en_sentential = cursor.getString(cursor.getColumnIndex("_en_sentential"));
+                String _cn_sentential = cursor.getString(cursor.getColumnIndex("_cn_sentential"));
+                result.add(new Yuliaoku(_id, _entry, _phonetic, _interpretation, _formation, _quotation, _translation, _author, _title, _source, _time, _en_sentential, _cn_sentential));
+            }
+        } else if (dictionary.toLowerCase().equals("Oxford".toLowerCase())) {
+            cursor = database.rawQuery("select * from " + dictionary + " where _key = ?", new String[]{key});
+            while (cursor.moveToNext()) {
+                long _id = cursor.getLong(cursor.getColumnIndex("_id"));
+                String _key = cursor.getString(cursor.getColumnIndex("_key"));
+                String _value = cursor.getString(cursor.getColumnIndex("_value"));
+                result.add(new Oxford(_id, _key, _value));
+            }
         }
-        cursor.close();
+        if (cursor != null) {
+            cursor.close();
+        }
         return result;
     }
 
-    public List<String> querylike(String key) {
+    public List<String> querylike(String key, String dictionary) {
         List<String> result = new ArrayList<>();
-        SQLiteDatabase database = open();
-        Cursor cursor = database.rawQuery("select _value from  main where _key like %?%", new String[]{key});
-        while (cursor.moveToNext()) {
-            result.add(cursor.getString(cursor.getColumnIndex("_value")));
+        Cursor cursor = null;
+        if (dictionary.toLowerCase().equals("yuliaoku".toLowerCase())) {
+            cursor = database.rawQuery("select _entry from " + dictionary + " where _entry like %?%", new String[]{key});
+            while (cursor.moveToNext()) {
+                result.add(cursor.getString(cursor.getColumnIndex("_value")));
+            }
+        } else if (dictionary.toLowerCase().equals("Oxford".toLowerCase())) {
+            cursor = database.rawQuery("select _key from " + dictionary + " where _key like %?%", new String[]{key});
+            while (cursor.moveToNext()) {
+                result.add(cursor.getString(cursor.getColumnIndex("_key")));
+            }
+        }
+        if (cursor != null) {
+            cursor.close();
         }
         return result;
     }
